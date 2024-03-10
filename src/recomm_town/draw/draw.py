6fd9b2a226d3c0ee7b712293d10a906e66d81a4e
Firemark <marpiechula@gmail.com>
2024-03-10 23:58:25 +0100

    Add RoundedRectangle

--- src/recomm_town/draw/draw.py
+++ src/recomm_town/draw/draw.py
@@ -9,11 +9,13 @@ from pyglet.window import Window
 from pyglet.text import Label
 
 from recomm_town.app import GuiGroup
+from recomm_town.draw.track_human import TrackHumanDraw
 from recomm_town.draw.utils import crop_label, to_color
 from recomm_town.shaders import AnimatedLine, Sprite
 from recomm_town.shaders.human_group import HumanGroup
 from recomm_town.common import Trivia, Vec
 from recomm_town.human import Human, Activity
+from recomm_town.shaders.rounded_rectangle import RoundedRectangle
 from recomm_town.town import PlaceFunction as PF
 from recomm_town.town.place import Place, Way
 
@@ -27,9 +29,6 @@ from recomm_town.draw.consts import (
     TEXTURES,
     FONT,
     COLORS,
-    LEVELS,
-    LEVEL_COLORS,
-    ACTIVITY_LABELS,
     ACTIVITY_TEXTURE_VARIANTS,
 )
 
@@ -95,14 +94,13 @@ class Draw:
                 anchor_y="top",
                 **kw_font,
             ),
-            BorderedRectangle(
+            RoundedRectangle(
                 x=0.0,
                 y=-300.0,
                 width=650.0,
                 height=300.0,
-                border=10,
+                round=32,
                 color=COLORS.dashboard_bg.to_pyglet_alpha(0.5),
-                border_color=COLORS.dashboard_text.to_pyglet(),
                 **kw,
             ),
         ]
@@ -305,173 +303,3 @@ class Draw:
             for obj in objs:
                 obj.delete()
 
-
-class TrackHumanDraw:
-    def __init__(self, human: Human, batch: Batch, group: GuiGroup, width: int):
-        self.batch = batch
-        self.group = group
-        self.human = human
-
-        human.level_observers["draw_track"] = self._level_update
-        human.activity_observers["draw_track"] = self._act_update
-        human.knowledge_observers["draw_track"] = lambda *a: self._trivia_update()
-        human.friend_observers["draw_track"] = lambda *a: self._friend_update()
-
-        kw = dict(batch=batch, group=group)
-        kw_font = dict(
-            **kw,
-            font_name=FONT,
-            font_size=14,
-            bold=True,
-            anchor_x="left",
-            anchor_y="top",
-        )
-
-        df_color = COLORS.dashboard_text.to_pyglet_alpha()
-        self.screen_width = width
-        x = width - 850.0
-        y = -300.0
-        self.objs = {
-            "background": BorderedRectangle(
-                x=x,
-                y=y,
-                width=850.0,
-                height=300.0,
-                border=10,
-                color=COLORS.dashboard_bg.to_pyglet_alpha(0.5),
-                border_color=COLORS.dashboard_text.to_pyglet(),
-                **kw,
-            ),
-            "labels": {
-                "name": Label(
-                    text=f"Name: {human.info.name}",
-                    x=x + 20.0,
-                    y=y + 280.0,
-                    color=df_color,
-                    **kw_font,
-                ),
-                "level": Label(
-                    text=f"Levels:",
-                    x=x + 20.0,
-                    y=y + 240.0,
-                    **kw_font,
-                ),
-                "levels": {
-                    level: Label(
-                        text=f"{level.title()}:",
-                        x=x + 20.0,
-                        y=y + 240.0 - 30.0 * index,
-                        color=(*LEVEL_COLORS[level], 255),
-                        **kw_font,
-                    )
-                    for index, level in enumerate(LEVELS, start=1)
-                },
-                "activity": Label(
-                    text="Activity:",
-                    x=x + 250.0,
-                    y=y + 280.0,
-                    color=df_color,
-                    **kw_font,
-                ),
-                "knowledge": Label(
-                    text="Knowledge:",
-                    x=x + 250.0,
-                    y=y + 240.0,
-                    color=df_color,
-                    **kw_font,
-                ),
-                "friend": Label(
-                    text="Friends:",
-                    x=x + 650.0,
-                    y=y + 240.0,
-                    color=df_color,
-                    **kw_font,
-                ),
-            },
-            "levels": {
-                level: Label(
-                    x=x + 150.0,
-                    y=y + 240.0 - 30.0 * index,
-                    color=(*LEVEL_COLORS[level], 255),
-                    **kw_font,
-                )
-                for index, level in enumerate(LEVELS, start=1)
-            },
-            "activity": Label(
-                x=x + 360.0,
-                y=y + 280.0,
-                **kw_font,
-            ),
-            "knowledge": Label(
-                x=x + 250.0,
-                y=y + 210.0,
-                multiline=True,
-                width=400.0,
-                **kw_font,
-            ),
-            "friends": Label(
-                x=x + 650.0,
-                y=y + 210.0,
-                multiline=True,
-                width=200.0,
-                **kw_font,
-            ),
-        }
-
-        self._trivia_update()
-        self._friend_update()
-        self._act_update(human.activity)
-        for level in LEVELS:
-            self._level_update(level, getattr(human.levels, level).value)
-
-    def on_resize(self, width: int):
-        self._translate(width, self.objs)
-        self.screen_width = width
-
-    def _translate(self, width: int, objs):
-        for obj in objs.values():
-            if isinstance(obj, dict):
-                self._translate(width, obj)
-            else:
-                obj.x += width - self.screen_width
-
-    def stop(self):
-        self.human.level_observers.pop("draw_track", None)
-        self.human.activity_observers.pop("draw_track", None)
-        self.human.knowledge_observers.pop("draw_track", None)
-        self.human.talk_observers.pop("draw_track", None)
-        self.human.friend_observers.pop("draw_track", None)
-        self.objs.clear()
-
-    def _level_update(self, attr: str, value: float):
-        self.objs["levels"][attr].text = f"{value * 100.0:3.0f} %"
-
-    def _act_update(self, activity: Activity):
-        self.objs["activity"].text = ACTIVITY_LABELS[activity].format()
-
-    def _trivia_update(self):
-        gen = (
-            (trivia, sum(chunks.values()))
-            for trivia, chunks in self.human.knowledge.items()
-        )
-        trivias = (
-            f"{index}. {crop_label(trivia.name, size=25):25} " f"{level * 100:3.0f}"
-            for (trivia, level), index in zip(
-                sorted(gen, key=lambda o: o[1], reverse=True),
-                range(1, 8 + 1),
-            )
-            if level > 1e-2
-        )
-        self.objs["knowledge"].text = "\n".join(trivias)
-
-    def _friend_update(self):
-        gen = self.human.friend_levels.items()
-        friends = (
-            f"{index}. {friend.info.name}"
-            for (friend, level), index in zip(
-                sorted(gen, key=lambda o: o[1], reverse=True),
-                range(1, 8 + 1),
-            )
-            if level > 1e-2
-        )
-        self.objs["friends"].text = "\n".join(friends)
---
+++ src/recomm_town/shaders/rounded_rectangle.py
@@ -0,0 +1,129 @@
+from math import pi, sin, cos
+
+from pyglet.graphics import Batch
+from pyglet.shapes import (
+    ShapeBase,
+    get_default_shader,
+    GL_SRC_ALPHA,
+    GL_ONE_MINUS_SRC_ALPHA,
+)
+
+
+class RoundedRectangle(ShapeBase):
+    def __init__(
+        self,
+        x,
+        y,
+        width,
+        height,
+        round,
+        round_verts=6,
+        color=(255, 255, 255, 255),
+        batch=None,
+        group=None,
+    ):
+        self._x = x
+        self._y = y
+        self._width = width
+        self._height = height
+        self._rotation = 0
+        self._round = round
+        self._round_verts = round_verts
+        self._num_verts = 5 * 6 + 4 * 3 * self._round_verts
+
+        r, g, b, *a = color
+        self._rgba = r, g, b, a[0] if a else 255
+
+        program = get_default_shader()
+        self._batch = batch or Batch()
+        self._group = self.group_class(
+            GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, program, group
+        )
+
+        self._create_vertex_list()
+        self._update_vertices()
+
+    def _create_vertex_list(self):
+        self._vertex_list = self._group.program.vertex_list(
+            self._num_verts,
+            self._draw_mode,
+            self._batch,
+            self._group,
+            colors=("Bn", self._rgba * self._num_verts),
+            translation=("f", (self._x, self._y) * self._num_verts),
+        )
+
+    def _update_vertices(self):
+        if not self._visible:
+            self._vertex_list.position[:] = (0, 0) * self._num_verts
+            return
+
+        r = self._round
+        x1 = -self._anchor_x + r
+        y1 = -self._anchor_y + r
+        x2 = x1 + self._width - 2 * r
+        y2 = y1 + self._height - 2 * r
+
+        corners = [
+            (x1, y1, -r, -r),
+            (x2, y1, +r, -r),
+            (x2, y2, +r, +r),
+            (x1, y2, -r, +r),
+        ]
+
+        position = []
+        for x, y, rx, ry in corners:
+            xo = x
+            yo = y + ry
+
+            for i in range(1, self._round_verts + 1):
+                xn = x + rx * sin(pi / 2 * i / self._round_verts)
+                yn = y + ry * cos(pi / 2 * i / self._round_verts)
+                # fmt: off
+                position += [
+                    xo, yo,
+                    xn, yn,
+                    x, y,
+                ]
+                # fmt: on
+                xo, yo = xn, yn
+
+        # fmt: off
+        position += [
+            x1, y1,
+            x2, y1,
+            x2, y2, 
+            x1, y1, 
+            x2, y2, 
+            x1, y2,
+            #
+            x1    , y1,
+            x1 - r, y1,
+            x1 - r, y2, 
+            x1    , y1, 
+            x1 - r, y2, 
+            x1    , y2,
+            #
+            x2    , y1,
+            x2 + r, y1,
+            x2 + r, y2, 
+            x2    , y1, 
+            x2 + r, y2, 
+            x2    , y2,
+            #
+            x1, y1,
+            x2, y1,
+            x2, y1 - r, 
+            x1, y1, 
+            x2, y1 - r, 
+            x1, y1 - r,
+            #
+            x1, y2,
+            x2, y2,
+            x2, y2 + r, 
+            x1, y2, 
+            x2, y2 + r, 
+            x1, y2 + r,
+        ]
+        # fmt: on
+        self._vertex_list.position[:] = position
